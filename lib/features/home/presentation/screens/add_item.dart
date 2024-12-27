4aef99a12769e7ce59d87491151d98c2380e410e
ahmedalnagdy18 <ahmedalnagdy188@gmail.com>
2024-12-27 20:30:28 +0200

    finx some error

--- lib/features/home/presentation/screens/add_item.dart
+++ lib/features/home/presentation/screens/add_item.dart
@@ -37,91 +37,93 @@ class _AddItemPageState extends State<AddItemPage> {
         padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
         child: Form(
           onChanged: _isEnabled,
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  InkWell(
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                    child: const Icon(
-                      Icons.arrow_back,
-                      size: 24,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Icon(
+                        Icons.arrow_back,
+                        size: 24,
+                      ),
                     ),
+                    const SizedBox(width: 20),
+                    const Text(
+                      "اضافة منتج",
+                      style: TextStyle(color: Colors.black, fontSize: 24),
+                    ),
+                  ],
+                ),
+                SizedBox(height: appHight(context, 0.10)),
+                Padding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: appHight(context, 0.50)),
+                  child: TextFieldWidget(
+                    mycontroller: _code,
+                    label: "الكود",
+                    inputFormatters: [
+                      FilteringTextInputFormatter.allow(RegExp(r'[0-9.,]')),
+                      FilteringTextInputFormatter.deny(RegExp(r'\s')),
+                    ],
                   ),
-                  const SizedBox(width: 20),
-                  const Text(
-                    "اضافة منتج",
-                    style: TextStyle(color: Colors.black, fontSize: 24),
+                ),
+                const SizedBox(height: 20),
+                Padding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: appHight(context, 0.50)),
+                  child: TextFieldWidget(
+                    mycontroller: _productName,
+                    label: "اسم المنتج",
                   ),
-                ],
-              ),
-              SizedBox(height: appHight(context, 0.10)),
-              Padding(
-                padding:
-                    EdgeInsets.symmetric(horizontal: appHight(context, 0.50)),
-                child: TextFieldWidget(
-                  mycontroller: _code,
-                  label: "الكود",
-                  inputFormatters: [
-                    FilteringTextInputFormatter.allow(RegExp(r'[0-9.,]')),
-                    FilteringTextInputFormatter.deny(RegExp(r'\s')),
-                  ],
                 ),
-              ),
-              const SizedBox(height: 20),
-              Padding(
-                padding:
-                    EdgeInsets.symmetric(horizontal: appHight(context, 0.50)),
-                child: TextFieldWidget(
-                  mycontroller: _productName,
-                  label: "اسم المنتج",
+                const SizedBox(height: 20),
+                Padding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: appHight(context, 0.50)),
+                  child: TextFieldWidget(
+                    mycontroller: _quantity,
+                    label: "الكمية",
+                    inputFormatters: [
+                      FilteringTextInputFormatter.allow(RegExp(r'[0-9.,]')),
+                      FilteringTextInputFormatter.deny(RegExp(r'\s')),
+                    ],
+                  ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              Padding(
-                padding:
-                    EdgeInsets.symmetric(horizontal: appHight(context, 0.50)),
-                child: TextFieldWidget(
-                  mycontroller: _quantity,
-                  label: "الكمية",
-                  inputFormatters: [
-                    FilteringTextInputFormatter.allow(RegExp(r'[0-9.,]')),
-                    FilteringTextInputFormatter.deny(RegExp(r'\s')),
-                  ],
+                const SizedBox(height: 20),
+                Padding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: appHight(context, 0.50)),
+                  child: TextFieldWidget(
+                    mycontroller: _price,
+                    label: "السعر",
+                    inputFormatters: [
+                      FilteringTextInputFormatter.allow(RegExp(r'[0-9.,]')),
+                      FilteringTextInputFormatter.deny(RegExp(r'\s')),
+                    ],
+                  ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              Padding(
-                padding:
-                    EdgeInsets.symmetric(horizontal: appHight(context, 0.50)),
-                child: TextFieldWidget(
-                  mycontroller: _price,
-                  label: "السعر",
-                  inputFormatters: [
-                    FilteringTextInputFormatter.allow(RegExp(r'[0-9.,]')),
-                    FilteringTextInputFormatter.deny(RegExp(r'\s')),
-                  ],
+                const SizedBox(height: 60),
+                Padding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: appHight(context, 0.50)),
+                  child: ColoredButtonWidget(
+                    buttonColor:
+                        !_isButtonEnabled ? AppColors.darkGrey : Colors.black,
+                    onPressed: _isButtonEnabled
+                        ? () {
+                            //       print("object");
+                          }
+                        : null,
+                    text: "Add",
+                    textColor: Colors.white,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 60),
-              Padding(
-                padding:
-                    EdgeInsets.symmetric(horizontal: appHight(context, 0.50)),
-                child: ColoredButtonWidget(
-                  buttonColor:
-                      !_isButtonEnabled ? AppColors.darkGrey : Colors.black,
-                  onPressed: _isButtonEnabled
-                      ? () {
-                          //       print("object");
-                        }
-                      : null,
-                  text: "Add",
-                  textColor: Colors.white,
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
